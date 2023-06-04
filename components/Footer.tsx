99181439771e60b29b2b5ef68ff457faf39d806a
thisishuzaifa <huzaifa_khalid97@outlook.com>
2023-06-04 14:03:27 -0700

    check deployement

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,7 +6,7 @@ export default function Footer() {
         <footer className="text-white-600 text-xl max-h">
         <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
           <a className="flex title-font font-medium items-center md:justify-start justify-center text-white-900">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-10 h-10 text-white p-2 bg-green-500 rounded-full" viewBox="0 0 24 24">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-10 h-10 text-white p-2 bg-green-500 rounded-full" viewBox="0 0 24 24">
               <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
             </svg>
             <span className="ml-3 text-xl">FSWD Course Site</span>
--- components/PostPreview.tsx
+++ components/PostPreview.tsx
@@ -4,7 +4,7 @@ import { PostMetadata } from './PostMetadata'
 export default function PostPreview({ post }: { post: PostMetadata }) {
     return (
         <div className="flex flex-col justify-center items-center">
-        <Link  className="text-2xl font-bold text-center" href={`/blog/${post.slug}`}>
+        <Link  className="text-2xl font-bold text-center" href={`blog/${post.slug}`}>
             {post.title}
         </Link>
         <div className="text-gray-600 text-sm">{post.date}</div>
