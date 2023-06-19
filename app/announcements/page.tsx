b2a69e4c7267a91ebf1f50c3753bd76675e1ffd7
thisishuzaifa <huzaifa_khalid97@outlook.com>
2023-06-18 20:26:17 -0400

    added announcements page

---
+++ app/announcements/page.tsx
@@ -0,0 +1,19 @@
+export default function Announcements() {
+
+    return(
+        <main className="flex flex-col items-center justify-between p-24">
+            <h3 className="text-4xl font-bold text-center"> Announcements </h3>
+            <p className="text-white-600 text-xl mt-4">
+                This page will updated with announcements as we proceed with the course.
+            </p>
+            <br />
+
+            <h5 className="text-5xl font-bold text-center text-emerald-600 mt-8 "> Announcement: Class resumes </h5>
+            <p className="text-white-600 text-xl mt-4">
+                Class resumes on Tuesday, September 22, 2023. You guys will be showing your assignments and we will do a small code review.
+            </p>
+
+            </main>
+
+    )
+}
