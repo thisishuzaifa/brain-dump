e1eb7b6fd3cf00d4e93173c54bef266d0e3cc229
thisishuzaifa <huzaifa_khalid97@outlook.com>
2023-07-27 08:15:49 -0700

    minor changes

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -29,9 +29,8 @@ export default function About(){
             </p>
             <br />
             <div className="flex flex-row justify-center space-x-4 items-center p-2 m-2">
-            <Link className="text-blue-600 text-xl "href="https://www.linkedin.com/muhammad-huzaifa-khalid">LinkedIn  </Link>
-            <Link className="text-red-600 text-xl"href="https://www.instagram.com/huzaifaa_khalid/"> Instagram </Link>
-            <Link className="text-emerald-600 text-xl"href="https://www.github.com/thisishuzaifa"> Github </Link>
+            <Link className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"href="https://www.linkedin.com/muhammad-huzaifa-khalid"> LinkedIn  </Link>
+            <Link className="text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 focus:outline-none dark:focus:ring-purple-800"href="https://www.github.com/thisishuzaifa"> Github </Link>
             </div>    
 
         </div>
@@ -45,26 +44,32 @@ export default function About(){
             <div className="grid grid-cols-1 gap-6">
             <h4 className="text-2xl font-bold text-start"> Personal Machine </h4>
             <ul className="text-emerald-500 text-l mt-4">
-                <li>Ryzen 7 5800x</li>
-                <li>32GB DDR4 3600Mhz</li>
-                <li>1TB NVME SSD</li>
-                <li>RTX 3060 Ti</li>
-                <li>Windows 11</li>
+                <li> Ryzen 7 5800x </li>
+                <li> 32GB DDR4 3600Mhz </li>
+                <li> 1TB NVME SSD </li>
+                <li> RTX 3060 Ti </li>
+                <li> Windows 11 </li>
             </ul>
             
             <h4 className="text-2xl font-bold text-start"> Dev Environment </h4>
             <ul className="text-emerald-500 text-l mt-4 ">
-                <li>WSL2</li>
-                <li>Ubuntu 20.04</li>
-                <li>VS Code</li>
+                <li> WSL2 </li>
+                <li> AWS </li>
+                <li> VS Code </li>
                 <li> Vercel </li>
                 <li> Github </li>
                 <li> Docker </li>
                 <li> NodeJS </li>
             </ul>
-            </div>
-        
 
+            <h4 className="text-2xl font-bold text-center mt-12"> Other random things I use </h4>
+            <ul className="text-emerald-500 text-l mt-4">
+                <li> M1 Macbook Pro  </li>
+                <li> Logitech C920 </li>
+                <li> WH-1000MX3 </li>
+                <li> Logitech MX Master 3 </li>
+            </ul>    
+            </div>
 
         </main>
     )
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-import Image from 'next/image'
-import Link from 'next/link'
 import getPostMetadata from '@/components/getPostMetadata';
 import PostPreview from '@/components/PostPreview';
 
@@ -17,14 +15,19 @@ export default function Home() {
       <div className="flex flex-col items-center justify-center m-4">
         <h1 className="p-2 text-5xl font-bold text-center">Welcome to the FSWD Course Site</h1>
         <br />
-        <p className="p-2 text-xl">You will find all course related conent as it is made available to you. We will cover different topics throughout the course and clear instructions will be given to you for assignments and other related projects.</p>
+        <p className="p-2 text-xl">
+        You will find all course related conent as it is made available to you.
+        We will cover different topics throughout the course and clear instructions will be given to you for assignments and other related projects.</p>
         <br />
       </div>
       <br />
       <div className="flex flex-col items-center justify-center m-2">
         {postPreviews}
       </div>
-      <p className="text-center text-white-600 text-m mt-8"> Please note that the tools and technologies I will use are opinionated, you are free to choose any other alternatives you see fit or are comfortable with. As long as it gets the job done it really does not matter, barring crippling performance issues.</p>
+      <p className="text-center text-white-600 text-m mt-8">
+      Please note that the tools and technologies I will use are opinionated,
+      you are free to choose any other alternatives you see fit or are comfortable with.
+      As long as it gets the job done it really does not matter, barring crippling performance issues.</p>
     </main>
     </>
   )
